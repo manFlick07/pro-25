dc785b9f7dfe467d58a0f532ae9e0d51738f1333
manFlick07 <83296562+manFlick07@users.noreply.github.com>
2021-08-21 14:49:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -62,10 +62,10 @@ function draw() {
 	
 	imageMode(CENTER);
 
-	image(paperImg,ball.position.y,ball.position.x,radius,radius);
+	//image(paperImg,ball.position.y,ball.position.x,radius,radius);
 	//image(paperImg,ball.position.x,ball.position.y,radius/2,radius/2);
 	//ellipse(ball.position.x,ball.position.y,radius,radius);
-	//image(paperImg,ball.position.x,ball.position.y,radius,radius);
+	image(paperImg,ball.position.x,ball.position.y,radius,radius);
 
 
 	
